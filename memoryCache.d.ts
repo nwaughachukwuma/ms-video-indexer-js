ee666e991ab69c4ca7d49be0edeba07061a5b733
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2021-03-14 22:02:15 +0400

    Add tsDoc to memoryCache

--- memoryCache.d.ts
+++ memoryCache.d.ts
@@ -1,5 +1,20 @@
 import { CacheHandlers, seconds } from './lib.msvi-api'
 
+/**
+ * A simple hashmap which serves as our cache
+ *
+ * @param ttl how long, in seconds to keep the cache object before it's deleted
+ *
+ * @example
+ * ```
+ * import memoryCache from "memoryCache"
+ *
+ * const cache =  memoryCache(60) // can hold for 60s
+ *
+ * cache.set('cache-key', {item: 'cache-item'})
+ *
+ * ```
+ */
 declare function memoryCache(ttl: seconds): CacheHandlers
 
 export default memoryCache
