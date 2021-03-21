d39a19922a08389e2e686f0c20ffc47900b0509f
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2021-03-21 13:44:49 +0400

    Add a much robust check for browser/node.js environment

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -2,6 +2,8 @@ import 'jest-matcher-one-of'
 
 jest.setTimeout(10000)
 
+// this is used to add fetch to {window|global}.fetch for jest which runs on a node.js environment
+// and yet satisfies the condition := typeof window !== 'undefined' && this === window
 if (!globalThis.fetch) {
   globalThis.fetch = require('node-fetch')
 }
--- platformFetch.js
+++ platformFetch.js
@@ -1,11 +1,22 @@
-let fetch
-if (typeof window !== 'undefined') {
-  fetch = window.fetch
-} else {
-  fetch = require('node-fetch')
-  if (!globalThis.fetch) {
-    globalThis.fetch = fetch
-  }
+function isBrowser() {
+  return typeof window !== 'undefined' && this === window
 }
 
+function platformFetch() {
+  if (!isBrowser()) {
+    const fetch = require('node-fetch')
+    if (!globalThis.fetch) {
+      globalThis.fetch = fetch
+    }
+    return fetch
+  }
+
+  return window.fetch
+}
+
+const fetch = platformFetch()
+
 module.exports = fetch
+exports.fetch = fetch
+// Needed for TypeScript consumers without esModuleInterop.
+exports.default = fetch
