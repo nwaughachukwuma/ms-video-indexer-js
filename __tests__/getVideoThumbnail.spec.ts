15e32fec2c172ca1993ba811301fbdc9f1f5fb12
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2021-03-21 08:59:44 +0400

    Add test for when a thumbnailId does not belong to the video

--- __tests__/getVideoThumbnail.spec.ts
+++ __tests__/getVideoThumbnail.spec.ts
@@ -16,7 +16,8 @@ describe.only('Video Thumbnail String', () => {
   describe('Can fetch video thumbnail from specified id', () => {
     const validIndexId = '2770eb7aca'
     const wrongIndexId = 'some-random-id'
-    const validThumbnailId = '8fcef147-8655-425d-b5e3-1062fd32838b'
+    const validThumbnailId = 'e39f8945-19da-43a0-9d4e-ea9e384729e6'
+    const validUnrelatedThumbnailId = '8fcef147-8655-425d-b5e3-1062fd32838b'
     const randomThumbnailId = 'some-random-thumbnail-id'
 
     it('Should fail to get video thumbnail when provided a wrong indexId', async () => {
@@ -49,7 +50,21 @@ describe.only('Video Thumbnail String', () => {
       expect(result.message).toContain('The request is invalid')
     })
 
-    test('Should get video thumbnail when provided the right video index and thumbnailId', async () => {
+    it('Should fail to get video thumbnail when provided an unrelated thumbnailId to a given video index', async () => {
+      const response = await msvpapi.getVideoThumbnail(
+        validIndexId,
+        validUnrelatedThumbnailId,
+      )
+
+      // console.log('Response :=', JSON.parse(response))
+      expect(response).toBeTruthy()
+
+      const result = JSON.parse(response)
+      expect(result.ErrorType).toBeTruthy()
+      expect(result.ErrorType).toBe('NOT_FOUND')
+    })
+
+    it('Should get video thumbnail when provided the right video index and thumbnailId', async () => {
       const response = await msvpapi.getVideoThumbnail(
         validIndexId,
         validThumbnailId,
@@ -60,8 +75,12 @@ describe.only('Video Thumbnail String', () => {
       expect(typeof response).toBe('string')
       expect(response.length).toBeGreaterThan(1)
 
-      const result = JSON.parse(JSON.stringify(response))
-      expect(result.message).toBeUndefined()
+      try {
+        const result = JSON.parse(response)
+        expect(result.message).toBeUndefined()
+      } catch (e) {
+        expect(e).toBeInstanceOf(SyntaxError)
+      }
     })
   })
 })
