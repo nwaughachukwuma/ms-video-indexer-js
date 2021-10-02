b69ac9b33b7fef73c3d2bde48739ac505226a56c
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-10-02 08:09:56 +0400

    Use jest.config; Add babel config

---
+++ jest.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['__tests__/*.spec.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!sma-cache)'],
+  reporters: ['default'],
+  testMatch: ['**/__tests__/**/?(*.)+(spec|test).[tj]s?(x)'],
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/__tests__/jest.setup.ts'],
+}
